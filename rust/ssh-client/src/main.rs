76e34293254a56881f6f9a460844ef37a88a8bed
Alexander <26022428+xakep71k@users.noreply.github.com>
2022-02-19 23:14:17 +0300

    .

--- rust/ssh-client/src/main.rs
+++ rust/ssh-client/src/main.rs
@@ -2,6 +2,8 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
+    pretty_env_logger::init();
+
     if std::env::args().len() != 2 {
         help();
         std::process::exit(1);
@@ -19,12 +21,67 @@ fn main() -> std::io::Result<()> {
     }
 
     let host = host.to_string() + ":22";
+    log::info!("connecting to host {}", host);
     let mut stream = TcpStream::connect(host)?;
 
+    /*
+     * receiving server banner
+     */
     let mut buf = vec![0; 1024];
     let size = stream.read(&mut buf)?;
-    println!("read size: {}", size);
+    match std::str::from_utf8(&buf[..size]) {
+        Ok(s) => {
+            log::info!("server banner: {}", s.trim());
+        }
+        Err(err) => {
+            log::error!("failed to convert to utf8: {}", err);
+        }
+    }
+
+    /*
+     * sending client banner
+     */
+
+    let banner = "SSH-2.0-xakep71k simple ssh\r\n";
+    if let Err(err) = stream.write(banner.as_bytes()) {
+        log::error!("failed to send banner: {}", err);
+    } else {
+        log::info!("client banner sent successfully: '{}'", banner.trim());
+    }
+
+    /*
+     * handling key exchange init package
+     */
+    let _size = stream.read(&mut buf)?;
+    let key_exchange_init = KeyExchangeInit::from_raw_data(&buf[..]);
+    log::info!(
+        "key exchange init package received: {:?}",
+        key_exchange_init
+    );
+
     Ok(())
+}
+
+#[derive(Debug)]
+struct KeyExchangeInit {
+    length: u32,
+    padding: u8,
+}
+
+impl KeyExchangeInit {
+    fn from_raw_data(data: &[u8]) -> KeyExchangeInit {
+        KeyExchangeInit {
+            length: extract_length(data),
+            padding: data[4],
+        }
+    }
+}
+
+fn extract_length(package: &[u8]) -> u32 {
+    (package[0] as u32) << 24
+        | (package[1] as u32) << 16
+        | (package[2] as u32) << 8
+        | (package[3] as u32)
 }
 
 fn help() {
